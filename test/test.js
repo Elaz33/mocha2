049fd6153629c87368bf20d93283bc8274f80a08
Elazar <elazrosenfeld@gmail.com>
2023-06-18 23:40:27 -0400

    first

---
+++ test/test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(4));
+        });
+    });
+});
+describe('Math',function(){
+    it('Should test if 3*3 = 9',function(){
+        assert.equal((3*3), 9)
+    })
+    it('Should test if (3-4) * 8 =-8',function(){
+        (3 - 4) * 8,-8
+    })
+})
